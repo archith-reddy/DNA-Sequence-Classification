c5c62610ded9d3992cae0e5b8481b0f06ec43452
Archith Reddy Muddam <30622896+archith1996@users.noreply.github.com>
2019-04-13 17:47:14 -0400

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+
+# import the dataset from uci Molecular Biology (Promoter Gene Sequences)
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/promoters.data'
+names = ['Class', 'id', 'Sequence']
+data = pd.read_csv(url, names = names)
+classes = data.loc[:, 'Class']
+#print(classes[:5])
+
+# generate list of DNA sequences
+sequences = list(data.loc[:, 'Sequence'])
+dataset = {}
+
+# loop through sequences and split into individual nucleotides
+for i, seq in enumerate(sequences):
+    nucleotides = list(seq)
+    nucleotides = [x for x in nucleotides if x != '\t']
+    nucleotides.append(classes[i])
+    dataset[i] = nucleotides
+
+# turn dataset into pandas DataFrame
+dframe = pd.DataFrame(dataset)
+#print(dframe)
+
+# transpose the DataFrame
+df = dframe.transpose()
+print(df.iloc[:5])
+
+# rename the last dataframe column to class
+df.rename(columns = {57: 'Class'}, inplace = True)
+#print(df.iloc[:5])
+df.describe()
+series = []
+for name in df.columns:
+    series.append(df[name].value_counts())
+
+info = pd.DataFrame(series)
+details = info.transpose()
+print(details)
+
+# we can't run machine learning algorithms on the data in 'String' formats. As a result, we need to switch
+# it to numerical data. This can easily be accomplished using the pd.get_dummies() function
+numerical_df = pd.get_dummies(df)
+numerical_df.iloc[:5]
+df = numerical_df.drop(columns=['Class_-'])
+df.rename(columns = {'Class_+': 'Class'}, inplace = True)
+print(df.iloc[:5])
+
+# Use the model_selection module to separate training and testing datasets
+from sklearn import model_selection
+
+# Create X and Y datasets for training
+X = np.array(df.drop(['Class'], 1))
+y = np.array(df['Class'])
+
+# define seed for reproducibility
+seed = 1
+
+# split data into training and testing datasets
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.25, random_state=seed)
+
+# import algorithms
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+
+# define scoring method
+scoring = 'accuracy'
+
+# Define models to train
+names = ["Nearest Neighbors", "Gaussian Process",
+         "Decision Tree", "Neural Net",
+         "Naive Bayes", "SVM Linear"]
+
+classifiers = [
+    KNeighborsClassifier(n_neighbors = 3),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    MLPClassifier(alpha=1),
+    GaussianNB(),
+    SVC(kernel = 'linear'),
+]
+
+models = zip(names, classifiers)
+
+# evaluate each model in turn
+results = []
+names = []
+
+"""
+for name, model in models:
+    print(name)
+    kfold = model_selection.KFold(n_splits=10, random_state = seed)
+    cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+"""
+for name, model in models:
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print(name)
+    print(accuracy_score(y_test, predictions))
+    print(classification_report(y_test, predictions))
+
+# Accuracy - ratio of correctly predicted observation to the total observations.
+# Precision - (false positives) ratio of correctly predicted positive observations to the total predicted positive observations
+# Recall (Sensitivity) - (false negatives) ratio of correctly predicted positive observations to the all observations in actual class - yes.
+# F1 score - F1 Score is the weighted average of Precision and Recall. Therefore, this score takes both false positives and false
